4decc96fac94e2357d2a94de0607f2734c461a75
zanknaf <48393526+zanknaf@users.noreply.github.com>
2023-08-06 17:12:24 +0200

    Avalanche Added

--- src/core/rpcs.ts
+++ src/core/rpcs.ts
@@ -1,5 +1,5 @@
 import { mainnet } from "wagmi";
-import { arbitrum, polygonMumbai } from "wagmi/chains";
+import { arbitrum, avalanche, polygonMumbai } from "wagmi/chains";
 
 // Used for speed testing defaults
 export const RPC_URLS = {
@@ -24,6 +24,13 @@ export const RPC_URLS = {
     "https://polygon-mumbai.g.alchemy.com/v2/demo",
     "https://matic-mumbai.chainstacklabs.com",
   ],
+  [avalanche.id]: [
+    "https://rpc.ankr.com/avalanche	",
+    "https://api.avax.network/ext/bc/C/rpc",
+    "https://avax.meowrpc.com",
+    "https://avalanche.public-rpc.com",
+    "https://1rpc.io/avax/c",
+  ],
 };
 
 // used for wallet seeding txs
@@ -31,4 +38,5 @@ export const DEFAULT_RPC_URL: Record<number, string> = {
   [mainnet.id]: "https://rpc.ankr.com/eth",
   [arbitrum.id]: "https://rpc.ankr.com/arbitrum",
   [polygonMumbai.id]: "https://rpc.ankr.com/polygon_mumbai",
+  [avalanche.id]: "https://rpc.ankr.com/avalanche",
 };
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,12 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient } from "wagmi";
-import { arbitrum, mainnet, polygonMumbai } from "wagmi/chains";
+import { arbitrum, mainnet, polygonMumbai,avalanche } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { DEFAULT_RPC_URL } from "./core/rpcs"
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, arbitrum, polygonMumbai],
+  [mainnet, arbitrum, polygonMumbai,avalanche],
   [
     // set default jsonrpc per chain, preferred over publicprovider which is unreliable
     jsonRpcProvider({
